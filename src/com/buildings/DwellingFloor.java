434233da57430ffdb67cd94d55458372b8ea001e
Arsenii Kuteinitsin <arsenii_k94@mail.ru>
2016-10-27 01:54:14 +0400

    Initial commit

---
+++ src/com/buildings/DwellingFloor.java
@@ -0,0 +1,117 @@
+package com.buildings;
+
+/**
+ * Created by Арсений on 09.10.2016.
+ */
+public class DwellingFloor {
+
+    private int countOfFlats;
+    private Flat [] flats;
+
+    public DwellingFloor(int countOfFlats){
+        flats=new Flat[countOfFlats];
+        for (int i = 0; i < countOfFlats; i++) {
+            flats[i]=new Flat();
+        }
+        this.countOfFlats=countOfFlats;
+    }
+
+    public DwellingFloor(Flat [] flats){
+        this.flats=flats;
+        countOfFlats= flats.length;
+    }
+
+    public int getCountOfFlats() {
+        return flats.length;
+    }
+
+    public int getFlatsSquare(){
+        int i=0;
+
+        for (Flat flat:flats) {
+            i+=flat.getSquare();
+        }
+        return i;
+    }
+
+    public int getFlatsRooms(){
+        int i=0;
+
+        for (Flat flat:flats) {
+            i+=flat.getRooms();
+        }
+        return i;
+    }
+
+    public Flat[] getFlats() {
+        return flats;
+    }
+
+    public Flat onNumber(int number) {
+        return flats[number];
+    }
+
+    public void editFlat(int number, Flat flat) {
+        flats[number]=flat;
+
+    }
+
+    public void addNewFlat(int number, Flat flat) {
+
+        if (number<=flats.length) {
+
+            Flat [] newFlats=new Flat[flats.length+1];
+
+            for (int j = 0; j < number; j++) {
+                newFlats[j]=flats[j];
+            }
+
+            newFlats[number]=flat;
+
+            for (int j = number+1; j < newFlats.length; j++) {
+                newFlats[j]=flats[j-1];
+            }
+
+
+
+            newFlats[newFlats.length-1]=flat;
+
+            flats=newFlats;
+
+            countOfFlats++;
+        }
+    }
+
+    public void deleteFlat(int number){
+
+        Flat [] newFlats = new Flat[countOfFlats-1];
+
+        if (number<=countOfFlats-1){
+            for (int i = 0; i <number; i++) {
+                newFlats[i]=flats[i];
+            }
+            for (int i = number; i <newFlats.length; i++) {
+                newFlats[i]=flats[i+1];
+            }
+
+            flats=newFlats;
+            countOfFlats--;
+        }
+    }
+
+    public Flat getBestSpace(){
+
+        Flat bigestSquareFlat=flats[0];
+
+        for (int i = 0; i <flats.length ; i++) {
+
+            if (flats[i].getSquare()>bigestSquareFlat.getSquare()){
+                bigestSquareFlat=flats[i];
+
+            }
+        }
+
+        return bigestSquareFlat;
+    }
+
+}
---
+++ src/com/buildings/Main.java
@@ -0,0 +1,120 @@
+package com.buildings;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Flat [] flats=new Flat [9];
+
+        Flat [] flats1=new Flat [9];
+
+        Flat [] flats2=new Flat [9];
+
+        int [] flats3={3,5,8,43,6,8,9,12,7};
+
+        DwellingFloor [] dwellingfloor = new DwellingFloor[4];
+
+        for (int i = 0; i <9; i++) {
+            if (i<4)
+                flats[i]=new Flat();
+            if((i>=3)&&(i<7))
+                flats[i]=new Flat(i*30);
+            if ((i>=6)&&(i<9))
+                flats[i]=new Flat(i*30,i*2);
+        }
+
+        for (int i = 0; i <9; i++) {
+            if (i<4)
+                flats1[i]=new Flat();
+            if((i>=3)&&(i<7))
+                flats1[i]=new Flat(i*20);
+            if ((i>=6)&&(i<9))
+                flats1[i]=new Flat(i*20,i*3);
+        }
+
+        for (int i = 0; i <9; i++) {
+            if (i<4)
+                flats2[i]=new Flat();
+            if((i>=3)&&(i<7))
+                flats2[i]=new Flat(i*20);
+            if ((i>=6)&&(i<9))
+                flats2[i]=new Flat(i*20,i*3);
+        }
+
+        for (int i = 0; i <4; i++) {
+            switch (i){
+                case 0:{
+                    dwellingfloor[i]=new DwellingFloor(flats);
+                    break;
+                }
+
+                case 1:{
+                    dwellingfloor[i]=new DwellingFloor(flats1);
+                    break;
+                }
+
+                case 2:{
+                    dwellingfloor[i]=new DwellingFloor(flats2);
+                    break;
+                }
+
+                case 3:{
+                    dwellingfloor[i]=new DwellingFloor(10);
+                    break;
+                }
+
+            }
+        }
+
+        Dwelling dwelling1 =new Dwelling(dwellingfloor);
+
+        Dwelling dwelling2 =new Dwelling(9,flats3);
+
+        System.out.println(dwelling1.getCountOfFloors());
+        System.out.println(dwelling2.getCountOfFloors());
+
+        System.out.println(dwelling1.getSquare());
+        System.out.println(dwelling2.getSquare());
+
+        System.out.println(dwelling1.getRooms());
+        System.out.println(dwelling2.getRooms());
+
+        System.out.println(Arrays.toString(dwelling1.getDwellingFloors()));
+        System.out.println(Arrays.toString(dwelling2.getDwellingFloors()));
+
+        System.out.println(dwelling1.getDfellingFloor(1));
+        System.out.println(dwelling2.getDfellingFloor(4));
+
+        dwelling1.editDwellingFloor(3,new DwellingFloor(6));
+        Flat [] flat4={new Flat(12)};
+        dwelling2.editDwellingFloor(5,new DwellingFloor(flat4));
+
+        System.out.println(dwelling1.getFlat(1));
+        System.out.println(dwelling2.getFlat(6));
+
+        dwelling1.editFlat(7, new Flat());
+        dwelling2.editFlat(7, new Flat());
+
+        dwelling1.addNewFlat(45, new Flat());
+        dwelling2.addNewFlat(7, new Flat());
+
+        System.out.println(dwelling1.getCountOfFlats());
+        System.out.println(dwelling2.getCountOfFlats());
+
+        dwelling1.deleteFlat(7);
+        dwelling2.deleteFlat(7);
+
+        System.out.println(dwelling1.getCountOfFlats());
+        System.out.println(dwelling2.getCountOfFlats());
+
+        System.out.println(dwelling1.getBestSpace());
+        System.out.println(dwelling2.getBestSpace());
+
+        System.out.println(Arrays.toString(dwelling1.getSortedFlats()));
+        System.out.println(Arrays.toString(dwelling2.getSortedFlats()));
+
+
+    }
+}
